c6fa82e44b5ccb8d9e458162c6c20d26320438e4
DulleyNitin <dulleythomas1234@gmail.com>
2026-02-18 21:26:33 +0530

    implemented a reusable grid system and rendered the chess board

--- src/core/app.cpp
+++ src/core/app.cpp
@@ -3,21 +3,26 @@
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_log.h>
+#include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_timer.h>
 
-Application::Application() {
+Application::Application() 
+  : mBoardRenderer()
+{
   mRunning = true;
   if(!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_Log("video initialization failed!");
   }
-  mWindow = SDL_CreateWindow("chess", 600, 600, SDL_WINDOW_RESIZABLE);
+  mWindow = SDL_CreateWindow("chess", 800, 800, SDL_WINDOW_RESIZABLE);
   if(!mWindow) {
     SDL_Log("window creation failed");
   }
-  mRenderer = SDL_CreateRenderer(mWindow, NULL);
+  mRenderer = SDL_CreateRenderer(mWindow, nullptr);
   if(!mRenderer) {
     SDL_Log("renderer creation failed");
   }
+  SDL_SetRenderLogicalPresentation(mRenderer, 800, 800, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 }
 
 Application::~Application() {
@@ -45,17 +50,44 @@ void Application::update() {
 }
 
 void Application::render() {
-
+  SDL_SetRenderDrawColor(mRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(mRenderer);
+  mBoardRenderer.drawBoard(mRenderer);
+  SDL_RenderPresent(mRenderer);
 }
 
-void Application::tick() {
+void Application::tick(float dt) {
   input();
   update();
   render();
 }
 
 void Application::mainLoop() {
+  const float targetFrameTime = 1.f/60.f; //target frame is 60 frames per second
+  unsigned int lastTickTime = SDL_GetTicks(), startFrameTime, endFrameTime;
+  unsigned int currentTime, lastTime = 0;// to get the fps
+  int fps=0; //temporary to calculate the fps
   while(mRunning) {
-    tick();
+    startFrameTime = SDL_GetTicks();
+    float dt = (startFrameTime - lastTickTime) / 1000.f; //delta time
+    lastTickTime = startFrameTime;
+
+    if(dt > 0.05f) {
+      dt = 0.05f;
+    }
+    tick(dt);
+    endFrameTime = SDL_GetTicks();
+    float frameTime = (endFrameTime - startFrameTime) / 1000.f;
+    if(frameTime < targetFrameTime) {
+      SDL_Delay( (targetFrameTime-frameTime) * 1000.f);
+    }
+    fps++;
+    currentTime = SDL_GetTicks();
+    if( currentTime > lastTime+1000 ) {
+      lastTime = currentTime;
+      SDL_Log("fps = %d", fps);
+      fps = 0;
+    }
+    
   }
 }
--- src/core/app.h
+++ src/core/app.h
@@ -4,18 +4,21 @@
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_render.h>
 
+#include "../rendering/boardRenderer.h"
+
 class Application {
 private:
   bool mRunning;
 public:
   SDL_Window *mWindow;
   SDL_Renderer *mRenderer;
+  BoardRenderer mBoardRenderer;
   Application();
   ~Application();
   void input();
   void update();
   void render();
-  void tick();
+  void tick(float);
   void mainLoop();
 };
 
